267dead553f78d20ff944e6bde82d8133c2b7cd7
Nashwyn <74958008+Rocket8900@users.noreply.github.com>
2024-02-12 17:21:27 +0800

    feat: dockerised project and db

--- repair/main.go
+++ repair/main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"repair/controllers"
+	"gorm.io/driver/mysql"
+	"repair/models"
+	"gorm.io/gorm"
 )
 
 func main() {
-	fmt.Print("this is a test")
+	dsn := "user:pass@tcp(db:3306)/repairDb?parseTime=true"
+    db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+    if err != nil {
+        panic("failed to connect database")
+    }
+
+    // Auto Migrate the database
+    db.AutoMigrate(&models.Repair{})
+
+
 	server := gin.Default()
 
 	//Routes 
